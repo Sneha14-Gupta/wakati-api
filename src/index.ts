1b2a8853e8674c65cff039a4d45ca950c04e1ecf
sneha <sneha1423@protonmail.com>
2025-01-24 20:45:24 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import index from '@/routes/index.routes'
 const app = createApp()
 configureOpenAPI(app)
 
-const routes=[index]
+const routes=[index,analyze,url,ai]
 routes.forEach(route=>{
   app.route('/',route)
 })
